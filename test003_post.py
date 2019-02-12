8a08418970d9fe40f56e00454655acdad1b69148
Mitsuharu Uemoto <uemoto@ccs.tsukuba.ac.jp>
2019-02-12 09:34:17 +0900

    ポストプロセス追加

---
+++ test003_post.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+import sys
+
+# mx, my, mz, nx, ny, nz, nb = map(int, sys.argv[1:])
+mx, my, mz, nx, ny, nz, nb = 2, 2, 4, 12, 14, 16, 2
+
+buf = np.empty([mx, my, mz, nx, ny, nz, nb], dtype=float)
+
+for i, (ix, iy, iz) in enumerate(np.ndindex(mx, my, mz)):
+    print("# Loading %d..." % i)
+    jx, jy, jz, jb, tmp = np.loadtxt("log$06d.txt" % i, dtype="int,int,int,int,float")
+    buf[ix, iy, iz, :, :, :, :] = tmp.reshape([nx, ny, nz, nb])
+
+print("# Writing test003_result.npz")
+np.savez_compressed("test003_result.npz", buf)
+
