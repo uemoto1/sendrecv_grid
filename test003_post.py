cc5faadd6cbf1da891ce2e2c379ba17ac5c6f2c2
Mitsuharu Uemoto <uemoto@ccs.tsukuba.ac.jp>
2019-02-12 09:39:55 +0900

    領域確保修正

--- test003_post.py
+++ test003_post.py
@@ -2,15 +2,15 @@
 import numpy as np
 import sys
 
-# mx, my, mz, nx, ny, nz, nb = map(int, sys.argv[1:])
-mx, my, mz, nx, ny, nz, nb = 2, 2, 4, 12, 14, 16, 2
+# mx, my, mz, nx, ny, nz, nb, nd = map(int, sys.argv[1:])
+mx, my, mz, nx, ny, nz, nb, nd = 2, 2, 4, 12, 14, 16, 2, 4
 
-buf = np.empty([mx, my, mz, nx, ny, nz, nb], dtype=float)
+buf = np.empty([mx, my, mz, nx+2*nd, ny+2*nd, nz+2*nd, nb], dtype=float)
 
 for i, (ix, iy, iz) in enumerate(np.ndindex(mx, my, mz)):
     print("# Loading %d..." % i)
     jx, jy, jz, jb, tmp = np.loadtxt("log%06d.txt" % i, dtype="int,int,int,int,float", unpack=True)
-    buf[ix, iy, iz, :, :, :, :] = tmp.reshape([nx, ny, nz, nb])
+    buf[ix, iy, iz, :, :, :, :] = tmp.reshape([nx+2*nd, ny+2*nd, nz+2*nd, nb])
 
 print("# Writing test003_result.npz")
 np.savez_compressed("test003_result.npz", buf)
