98490bcc8d8bed11616dc2e8b7942cef54e29a34
Mitsuharu Uemoto <uemoto@ccs.tsukuba.ac.jp>
2019-02-12 10:55:40 +0900

    スクリプト修正

--- test002_post.py
+++ test002_post.py
@@ -5,12 +5,20 @@ import sys
 # mt, mx, my, mz, nx, ny, nz, nb, nd = map(int, sys.argv[1:])
 mt, mx, my, mz, nx, ny, nz, nb, nd = 2, 4, 1, 1, 12, 14, 16, 10, 4
 
-buf = np.empty([mt, mx, my, mz, nx+2*nd, ny+2*nd, nz+2*nd, nb], dtype=float)
+output = "test002_result.npz"
 
+ox, oy, oz = nx + 2 * nd, ny + 2 * nd, nz + 2 * nd
+buf = np.empty([mt, mx, my, mz, ox, oy, oz, nb], dtype=float)
 for i, (it, ix, iy, iz) in enumerate(np.ndindex(mt, mx, my, mz)):
     print("# Loading %d..." % i)
     jx, jy, jz, jb, tmp = np.loadtxt("log%06d.txt" % i, dtype="int,int,int,int,float", unpack=True)
-    buf[it, ix, iy, iz, :, :, :, :] = tmp.reshape([nx+2*nd, ny+2*nd, nz+2*nd, nb])
+    buf[it, ix, iy, iz, :, :, :, :] = tmp.reshape([mt, mx, my, mz, ox, oy, oz, nb])
 
-np.savez_compressed("test002_result.npz", buf)
-
+print("# Writing %s" % output)
+np.savez_compressed(
+    output, 
+    result=buf,
+    x=np.arange(1-nd, nx+nd+1),
+    y=np.arange(1-nd, ny+nd+1),
+    z=np.arange(1-nd, nz+nd+1),
+)
